761e2a24532b3d3c106fccf29a7c6e99b1a503b9
Lucas Gilmar da Silva <140455332+K0yall@users.noreply.github.com>
2024-09-12 13:17:56 -0300

    Delete Banco de Dados SQL do Zero ao Avançado (Andre Iacono) directory

--- Banco de Dados SQL do Zero ao Avançado (Andre Iacono)/4_In.sql
+++
@@ -1,7 +0,0 @@
--- 4. USANDO IN PARA FILTRAR MÚLTIPLOS VALORES
-USE sakila;
-
--- Seleciona todos os endereços em distritos específicos: Alberta, Texas ou Califórnia.
-SELECT * 
-FROM address
-WHERE district IN ('Alberta', 'Texas', 'California');
--- Banco de Dados SQL do Zero ao Avançado (Andre Iacono)/7_Limit.sql
+++
@@ -1,7 +0,0 @@
--- 7. PAGINAÇÃO COM LIMIT
-USE sakila;
-
--- Seleciona 10 registros a partir do 100º registro. Útil para paginação de resultados.
-SELECT * 
-FROM actor
-LIMIT 99, 10;
